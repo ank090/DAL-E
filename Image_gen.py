f0eed400f04ad19de5472f771b308e18a5a30845
ank090 <ankver090@gmail.com>
2022-12-14 00:01:58 +0530

    first commit

---
+++ Image_gen.py
@@ -0,0 +1,27 @@
+import os
+import openai
+import base64
+import argparse
+openai.api_key = os.getenv("OPEN_AI_API_KEY")
+
+#initiate parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--prompt", help = "Insert Text to genrate image.", default= "baby panda with gun in high detail")
+parser.add_argument("-n", "--number", help = "Enter number of images required", default = 2)
+parser.add_argument("-s", "--size", help = "Size of image", default = 256)
+
+#Reading arguments from command line 
+args = parser.parse_args()
+
+res = openai.Image.create(
+    prompt = args.prompt,
+    n = int(args.number),
+    size=f"{args.size}x{args.size}",
+    response_format = "b64_json"
+)
+
+for i in range(len(res['data'])):
+    b64_blob = res['data'][i]['b64_json']
+    print("Genrating Image...")
+    with open(f"image_gen{i+1}.jpg","wb") as file:
+        file.write(base64.urlsafe_b64decode(b64_blob))
